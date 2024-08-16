690051f2c9f6d73106b3cf39fb209f16f3c492f7
gitfarrah <125498363+gitfarrah@users.noreply.github.com>
2024-08-16 18:13:45 +0300

    removefromcart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import {cart} from '../data/cart.js';
+import {cart , removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
-import{formatCurrency} from './utils/money.js'
+import{formatCurrency} from './utils/money.js';
+
 
 
 let cartSummaryHTML ='';
@@ -17,7 +18,6 @@ cart.forEach((cartItem) =>{
     });
 
     cartSummaryHTML +=
-
     `
     <div class="cart-item-container">
         <div class="delivery-date">
@@ -42,8 +42,7 @@ cart.forEach((cartItem) =>{
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
-                Delete
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">Delete
                 </span>
             </div>
             </div>
@@ -97,4 +96,12 @@ cart.forEach((cartItem) =>{
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+.forEach((link) => {
+    link.addEventListener('click' , () => {
+        const productId = link.dataset.productId;
+         removeFromCart(productId); 
+    });
+});
 
