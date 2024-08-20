afe945636e3250bb5bdbe16e272254a1a4389b5f
gitfarrah <125498363+gitfarrah@users.noreply.github.com>
2024-08-20 16:37:21 +0300

    removing from cart button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -19,7 +19,7 @@ cart.forEach((cartItem) =>{
 
     cartSummaryHTML +=
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Wednesday, June 15
         </div>
@@ -101,7 +101,8 @@ document.querySelectorAll('.js-delete-link')
 .forEach((link) => {
     link.addEventListener('click' , () => {
         const productId = link.dataset.productId;
-         removeFromCart(productId); 
+         removeFromCart(productId);
+         console.log(cart);
     });
 });
 
