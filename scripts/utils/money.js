6061bae2356e3692ac91af719e91c76887723cba
gitfarrah <125498363+gitfarrah@users.noreply.github.com>
2024-08-25 19:35:27 +0300

    remove default tests and add money tests

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,6 +1,5 @@
-export function formatCurrency(priceCents){
-    return (priceCents / 100).toFixed(2);
-}
-
-// works only exporting one function
-export default formatCurrency;
+export function formatCurrency(priceCents) {
+    return (Math.round(priceCents) / 100).toFixed(2);
+  }
+  
+  export default formatCurrency;
