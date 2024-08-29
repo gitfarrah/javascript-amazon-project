3362dcfc74e6a7b75158b63f8585a270d87f7d70
gitfarrah <125498363+gitfarrah@users.noreply.github.com>
2024-08-29 14:34:56 +0300

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,38 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 
 
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
 
-loadProducts(()=>{
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value2');    
+        });
+    })
+
+]).then((values) => {
+    console.log(values)
     renderOrderSummary();
     renderPaymentSummary();
-
 });
+ 
+
+
+
+
+
+/*
+loadProducts(()=>{
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    }); 
+});
+*/
